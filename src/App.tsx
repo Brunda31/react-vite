e52b04f74b9fd264eb813c32159cff100ae19c85
brunda <lakkireddybrunda@gmail.com>
2024-06-04 16:18:33 -0500

    buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import Alert from "./components/Alert";
+import Buttons from "./components/buttons";
 
 function App() {
   return (
     <div>
-      <Alert>Hello World</Alert>
+      <Buttons color="secondary" onClick={() => console.log("Clicked")}>
+        My Button
+      </Buttons>
     </div>
   );
 }
