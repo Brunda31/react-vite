4fa213e7951d45ca27609ce819768ec135e72bb5
brunda <lakkireddybrunda@gmail.com>
2024-09-27 15:03:53 -0700

    formcommit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import { useState } from "react";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
-    <ExpandableText>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-      recusandae quia quisquam reprehenderit nemo molestias reiciendis iure?
-      Aperiam enim perferendis voluptate rerum nam natus. Quasi similique fuga
-      modi! Omnis sit consectetur aliquid sapiente corrupti rerum deserunt eaque
-      harum exercitationem reiciendis debitis tenetur, expedita optio at
-      veritatis quibusdam minima delectus nulla dolores! Ipsum error quis
-      reiciendis suscipit repellendus inventore recusandae, aliquam incidunt,
-      tenetur ut voluptatem, quos ea. Fuga repellat corporis excepturi aliquam
-      doloremque adipisci, quis, veritatis, cum blanditiis maxime enim impedit.
-      Libero fugit saepe eveniet deserunt obcaecati est eaque vero fuga pariatur
-      delectus voluptate ut dignissimos corporis omnis architecto maxime nulla
-      perspiciatis adipisci voluptatem, veritatis necessitatibus illum molestiae
-      odit quia. Labore veniam quisquam incidunt eveniet et maxime nulla ut,
-      sunt aperiam sed deleniti eaque sit, expedita molestiae officia omnis
-      culpa a, sint ipsa! Amet ipsum natus tempora totam, porro explicabo quia
-      exercitationem unde! Corporis error recusandae provident
-    </ExpandableText>
+    <div>
+      <Form />
+    </div>
   );
 }
 
